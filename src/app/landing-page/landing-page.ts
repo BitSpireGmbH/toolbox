59850b30b82aa7f23d71408d54e6dd9b8e8a21a1
Steven Giesel <stgiesel35@gmail.com>
2026-01-01 20:17:51 +0100

    fix: Remove scrollbar issue

--- src/app/landing-page/landing-page.ts
+++ src/app/landing-page/landing-page.ts
@@ -179,7 +179,6 @@ import { RouterLink } from '@angular/router';
               target="_blank"
               aria-label="Report bugs or request features on GitHub (opens in a new tab)">
               <span>Report it here</span>
-              <span class="sr-only"> (opens in new tab)</span>
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-3 h-3 flex-shrink-0" aria-hidden="true">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
               </svg>
