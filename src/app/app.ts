37e549014450987ebfd317dc6119ae3d53be67d9
Steven Giesel <stgiesel35@gmail.com>
2026-01-03 22:04:01 +0100

    feat: Use commit hash

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,17 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { environment } from '../environments/environment';
 import { NavmenuComponent } from './navmenu/navmenu.component';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, NavmenuComponent],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('toolbox');
-  protected readonly version = signal(environment.version);
   protected readonly sidebarOpen = signal(false);
 
   protected toggleSidebar(): void {
--- src/app/navmenu/navmenu.component.ts
+++ src/app/navmenu/navmenu.component.ts
@@ -1,18 +1,20 @@
-import { Component, ChangeDetectionStrategy, input, output } from '@angular/core';
+import { Component, ChangeDetectionStrategy, input, output, signal } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { environment } from '../../environments/environment';
 
 @Component({
   selector: 'app-navmenu',
   imports: [RouterLink, RouterLinkActive],
   templateUrl: './navmenu.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class NavmenuComponent {
   readonly sidebarOpen = input.required<boolean>();
-  readonly version = input.required<string>();
 
   readonly toggleSidebar = output<void>();
   readonly closeSidebar = output<void>();
+
+  protected readonly version = signal(environment.version);
 
   protected onToggleSidebar(): void {
     this.toggleSidebar.emit();
